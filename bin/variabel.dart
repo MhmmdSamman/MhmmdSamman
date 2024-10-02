6f42fa25b789c81a3f3c2a576575e0030219e397
Muhammad Samman <mhmmdsman790@gmail.com>
2024-10-02 18:30:53 +0800

    Add files via upload

---
+++ bin/variabel.dart
@@ -0,0 +1,16 @@
+//tanpa variable
+void main(){
+print('Muhammad Samman');
+
+//variable
+String nama;
+nama =  'Muhammad Samman';
+print(nama);
+
+
+//deklarasi variabel langsung
+String namaSaya = 'Muhammad Samman';
+print(namaSaya);
+}
+
+
