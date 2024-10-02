8c2e662793ad503b4258556b4ad82f53c7434dc9
Muhammad Samman <mhmmdsman790@gmail.com>
2024-10-02 18:26:17 +0800

    operator_logika

---
+++ bin/operator_logika.dart
@@ -0,0 +1,20 @@
+//operator logika adalah operator untul dua buah bool
+
+void main(){
+  bool a = true;
+  bool b = false;
+
+  if (a && b) { //AND
+    print("Keduanya true");
+  } 
+
+  if (a || b) { //OR
+    print("Setidaknya salah satu true"); 
+  }
+  if (!a) { //NOT
+    print("a false");
+  } else {
+    print("a true"); 
+  }
+
+}
