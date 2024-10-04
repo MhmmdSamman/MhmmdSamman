5d5f683bb84c011a691e1097f1c64df9d44918a6
Muhammad Samman <mhmmdsman790@gmail.com>
2024-10-04 17:59:02 +0800

    Tugas 1

---
+++ bin/map.dart
@@ -0,0 +1,17 @@
+//map = key
+//key bisa ditentukan dengan tipe data apapun
+
+//membuat map
+//Map<String, String> person = {};
+//var product = Map<String, String>();
+//var address = <String, String>{};
+void main(){
+//manipulasi map
+var name = <String, String>{};
+name['first'] = 'Mhmmd';
+name['last'] = 'Samman';
+
+name['first'] = 'Muhammad';//mengubah data
+print(name);
+//name.remove(value);//remove
+}
---
+++ bin/symbol.dart
@@ -0,0 +1,9 @@
+//symbol bisa digunakan sebagai constant namun jarang di pakai
+
+void main(){
+  Symbol symbol1 = Symbol('Muhammad Samman');
+  var symbol2 = #belajar;
+
+  print(symbol1);
+  print(symbol2);
+}
